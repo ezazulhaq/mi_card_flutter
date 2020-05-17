9fc48c128f07f9353b681559ea2581fa697226a7
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2020-05-17 15:28:29 +0530

    Adding Material Icon with Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
-                  radius: 60.0,
+                  radius: 40.0,
                   backgroundImage: AssetImage("images/ezazul.jpg"),
                 ),
                 Text(
                   "Ezazul Haq",
                   style: TextStyle(
-                    fontSize: 50.0,
+                    fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontFamily: "Pacifico",
@@ -37,6 +37,56 @@ class MyApp extends StatelessWidget {
                     fontFamily: "Source Sans Pro",
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "+91 9930800797",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "Source Sans Pro",
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "ezazulhaq.it@gmail.com",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "Source Sans Pro",
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
